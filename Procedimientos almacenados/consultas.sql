ed3af4b85b7dd7678cf92636b9f121fcee2d67c4
lFerchol <cadesluisxTo22@gmail.com>
2018-03-29 02:10:57 -0600

    Consultas
    Ejemplos de algunas consutas realizadas con los procedimientos almacenados

---
+++ Procedimientos almacenados/consultas.sql
@@ -0,0 +1,16 @@
+-- select busca_nombre_genero('Aventura')
+-- insert into pelicula values ('003','Blade 3','Accion')
+--select * from pelicula
+--select nombre into nombre_producto from pelicula where genero='Aventura'
+
+-- select * from boleto;
+-- insert into boleto values (2,'002','1996-08-28','1996-08-28 10:23:54+02',2.59,2,2) 
+
+--select * from taquilla
+--insert into taquilla values (2,2)
+
+--insert into vendedor values (3,'Santiago','3ra','99')
+--select  ordenes_fecha('1996-08-28','1996-08-28')
+
+select entradas_de_vendedor(1,'1996-08-28','1996-08-28')
+--select * from boleto
